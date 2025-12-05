310b0ea970b71f3bb7ba0abfe3a4a7f621fc5da6
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-12-05 14:02:54 -0500

    product service class

---
+++ F25W13FinalExamReview/Services/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using F25W13FinalExamReview.Models;
+using F25W13FinalExamReview.Data;
+
+namespace F25W13FinalExamReview.Services
+{
+    public class ProductService
+    {
+        private readonly ProductContext _context;
+
+        public ProductService(ProductContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<List<Product>> GetProductsAsync(string? searchKeyword = null)
+        {
+            // get all products
+            var products = _context.Products
+                                   .Include(p => p.Category)    // includes category of each product
+                                   .AsQueryable();
+
+            // get products by name
+            if (!string.IsNullOrWhiteSpace(searchKeyword))
+            {
+                products = products.Where(p => p.ProductName.Contains(searchKeyword));
+            }
+
+            return await products.ToListAsync();
+        }
+
+        public async Task<List<Product>> GetProductsByCategoryAsync(int catId)
+        {
+            // get all products
+            var products = _context.Products
+                                   .Include(p => p.Category)    // includes category of each product
+                                   .AsQueryable();
+
+            // get products by catId
+            if (catId > 0)
+            {
+                products = products.Where(p => p.CategoryId == catId);
+            }
+
+            return await products.ToListAsync();
+        }
+    }
+}
